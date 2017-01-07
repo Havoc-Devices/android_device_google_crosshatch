2c3d48f917f3e2642325a5301f294b36ca26ae27
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-07 17:04:12 -0600

    crosshatch: build com.android.ims.rcsmanager
    Change-Id: I89bf0b1a97bdb6f9dc2af6bb107ba5a61443ca68
    Signed-off-by: Joe Maples <joe@frap129.org>

--- device.mk
+++ device.mk
@@ -242,6 +242,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
 
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager
+
 # power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.pixel-libperfmgr
